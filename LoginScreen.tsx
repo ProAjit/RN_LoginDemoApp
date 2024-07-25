ace35844e25608fd8f4eeb18f8fdbab6e0b472bb
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-07-25 18:21:14 +0530

    Integrated activity indicator

--- LoginScreen.tsx
+++ LoginScreen.tsx
@@ -7,11 +7,12 @@
 
 import React, { useState } from 'react';
 import { View, Text, TextInput, KeyboardAvoidingView, ScrollView, Platform} from 'react-native';
-import { TouchableOpacity} from 'react-native';
+import { TouchableOpacity, ActivityIndicator} from 'react-native';
 // import navStyle from './styles/navStyle';
 import componentStyle from './styles/componentStyle';
 import { ImageBackground } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { red } from 'react-native-reanimated/lib/typescript/Colors';
 // import { useNavigation } from '@react-navigation/native';
 
 // const handlePress = (navigator) => {
@@ -23,6 +24,15 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const LoginScreen = (props) => {
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
+  const [show, setShow] = useState(false)
+  const displayLoader = () => {
+    setShow(true)
+    setTimeout(()=> {
+      setShow(false)
+      props.navigation.navigate("Home", {name})
+    }, 3000)
+  }
+
   // const navigator = useNavigation()
 
   return (
@@ -54,14 +64,16 @@ const LoginScreen = (props) => {
       />
      </View>
 
-     {/* <Button title='Login' 
-     color="#841584" 
+     {/* <Button title='Login'
+     color="#841584"
      onPress={()=> props.navigation.navigate("Home")}>
      </Button> */}
+
+     <ActivityIndicator size={60} color={"red"} animating={show} />
     
-     <TouchableOpacity style={componentStyle.button} onPress={()=> props.navigation.navigate("Home", {name})}>
+     <TouchableOpacity style={componentStyle.button} onPress={()=> displayLoader()}>
         <Text style={componentStyle.buttonText}>Login</Text>
-     </TouchableOpacity> 
+     </TouchableOpacity>
 
      </ImageBackground>
     </View>
