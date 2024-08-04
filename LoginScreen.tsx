d2fffe8b28c579fdc85a4a82a9ed9459c655265b
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-08-04 12:10:42 +0530

    Added Overlay Activity Indicator

--- LoginScreen.tsx
+++ LoginScreen.tsx
@@ -1,16 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, { useState } from 'react';
-import { View, Text, TextInput, KeyboardAvoidingView, ScrollView, Platform} from 'react-native';
-import { TouchableOpacity, ActivityIndicator, Image, StyleSheet} from 'react-native';
+import { View, Text, TextInput, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
+import { TouchableOpacity, Image, StyleSheet } from 'react-native';
 import componentStyle from './styles/componentStyle';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { NetworkStatusProvider, useNetworkStatus } from './Reachability/NetworkStatusContext';
+import OverlayActivityIndicator from './Utilities/OverlayActivityIndicator';
 
 const NetworkComponent: React.FC = () => {
   const { isConnected } = useNetworkStatus();
@@ -22,71 +16,67 @@ const NetworkComponent: React.FC = () => {
   );
 };
 
-
 const LoginScreen = (props: { navigation: { navigate: (arg0: string, arg1: { name: string; }) => void; }; }) => {
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
-  const [show, setShow] = useState(false)
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [show, setShow] = useState(false);
 
   const displayLoader = () => {
-    setShow(true)
-    setTimeout(()=> {
-      setShow(false)
-      props.navigation.navigate("Home", {name})
-    }, 3000)
-  }
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+      props.navigation.navigate("Home", { name });
+    }, 3000);
+  };
 
   const forgotPasswordPressed = () => {
-    setShow(true)
-    setTimeout(()=> {
-      setShow(false)
-    }, 3000)
-  }
+    setShow(true);
+    setTimeout(() => {
+      setShow(false);
+    }, 3000);
+  };
 
   return (
-    // <SafeAreaView>
-    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}
-    style={componentStyle.container}>
-    <ScrollView contentContainerStyle={componentStyle.scrollView}>
-    <NetworkStatusProvider>
-    <NetworkComponent/>
-    <View style={componentStyle.container}>
-    <View style={logoStyles.outerContainer}>
-      <View style={logoStyles.innerContainer}>
-        <Image
-          source={require('/Users/ajitsatarkar/Documents/React_Native_Git/RN_LoginPOC/RN_LoginDemoApp/images/login/logo.png')}
-          style={logoStyles.logo}
-        />
-      </View>
-    </View>
-    <View style={componentStyle.innerView}>
-      <Text style={componentStyle.textHeader}> MNG-HA Employee Self-Service </Text>
-      <Text style={componentStyle.text}> Username </Text>
-      <TextInput
-      style={componentStyle.inputText}
-      placeholder="Enter User Name here"
-      onChangeText={(text)=>setName(text)}
-      />
-      <Text style={componentStyle.text}> Password </Text>
-      <TextInput
-      style={componentStyle.inputText}
-      secureTextEntry={true}
-      placeholder="Enter your password"
-      onChangeText={setPassword}
-      />
-     <ActivityIndicator size={'large'} color={"darkgray"} animating={show}/>
-     <TouchableOpacity style={componentStyle.loginButton} onPress={()=> displayLoader()}>
-      <Text style={componentStyle.buttonText}>Login</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style={componentStyle.forgotButton} onPress={()=> forgotPasswordPressed()}>
-        <Text style={componentStyle.forgotButtonText}>Forgot Password</Text>
-     </TouchableOpacity>
-     </View>
-     </View>
-    </NetworkStatusProvider>
-    </ScrollView>
-   </KeyboardAvoidingView>
-  //  </SafeAreaView>
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={componentStyle.container}>
+      <ScrollView contentContainerStyle={componentStyle.scrollView}>
+        <NetworkStatusProvider>
+          <NetworkComponent />
+          <View style={componentStyle.container}>
+            <View style={logoStyles.outerContainer}>
+              <View style={logoStyles.innerContainer}>
+                <Image
+                  source={require('/Users/ajitsatarkar/Documents/React_Native_Git/RN_LoginPOC/RN_LoginDemoApp/images/login/logo.png')}
+                  style={logoStyles.logo}
+                />
+              </View>
+            </View>
+            <View style={componentStyle.innerView}>
+              <Text style={componentStyle.textHeader}>MNG-HA Employee Self-Service</Text>
+              <Text style={componentStyle.text}>Username</Text>
+              <TextInput
+                style={componentStyle.inputText}
+                placeholder="Enter User Name here"
+                onChangeText={(text) => setName(text)}
+              />
+              <Text style={componentStyle.text}>Password</Text>
+              <TextInput
+                style={componentStyle.inputText}
+                secureTextEntry={true}
+                placeholder="Enter your password"
+                onChangeText={setPassword}
+              />
+              <TouchableOpacity style={componentStyle.loginButton} onPress={displayLoader}>
+                <Text style={componentStyle.buttonText}>Login</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={componentStyle.forgotButton} onPress={forgotPasswordPressed}>
+                <Text style={componentStyle.forgotButtonText}>Forgot Password</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </NetworkStatusProvider>
+      </ScrollView>
+      <OverlayActivityIndicator show={show} />
+    </KeyboardAvoidingView>
   );
 };
 
---
+++ Utilities/OverlayActivityIndicator.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+
+const OverlayActivityIndicator = ({ show }) => {
+  return (
+    show && (
+      <View style={styles.overlay}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    )
+  );
+};
+
+const styles = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    zIndex: 1,
+  },
+});
+
+export default OverlayActivityIndicator;
