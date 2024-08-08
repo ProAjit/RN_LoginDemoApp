945f923fab56b331d35a14208db68345a78ff730
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-08-08 20:38:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './navigation/StackNavigator'
+import App from './Navigation/StackNavigator'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
