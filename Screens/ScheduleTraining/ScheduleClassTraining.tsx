9e97effa07d2996314bc55de89198ac1b66e777d
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-08-25 09:34:20 +0530

    Date range fixes

--- Screens/ScheduleTraining/ScheduleClassTraining.tsx
+++ Screens/ScheduleTraining/ScheduleClassTraining.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, ActivityIndicator, KeyboardAvoidingView} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, ActivityIndicator, KeyboardAvoidingView } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { submitTrainingData } from '../../Networking/ClassTrainingServices';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -7,12 +7,12 @@ import SegmentedControl from '@react-native-segmented-control/segmented-control'
 import TrainingList from './TrainingList';
 
 const TrainingDataArr = [
-  { noOfTrainees: 111111, department: 'Department 1', supervisor: 'Supervisor 1', location: 'Riyadh office', status: 'Accepted', fromDate: '20 Aug 2024 10:30 AM', toDate: '21 Aug 2024 11:30 AM'},
-  { noOfTrainees: 222222, department: 'Department 2', supervisor: 'Supervisor 2', location: 'Pune office', status: 'Under Review', fromDate: '21 Aug 2024 10:30 AM', toDate: '25 Aug 2024 11:30 AM'},
-  { noOfTrainees: 333333, department: 'Department 3', supervisor: 'Supervisor 3', location: 'Noida Office', status: 'Submitted', fromDate: '22 Aug 2024 10:30 AM', toDate: '26 Aug 2024 11:30 AM'},
-  { noOfTrainees: 444444, department: 'Department 4', supervisor: 'Supervisor 4', location: 'India office', status: 'Approved', fromDate: '23 Aug 2024 10:30 AM', toDate: '27 Aug 2024 11:30 AM'},
-  { noOfTrainees: 555555, department: 'Department 5', supervisor: 'Supervisor 5', location: 'UAE office', status: 'Closed', fromDate: '24 Aug 2024 10:30 AM', toDate: '28 Aug 2024 11:30 AM'},
-  { noOfTrainees: 666666, department: 'Department 6', supervisor: 'Supervisor 6', location: 'KSA Office', status: 'Rejected', fromDate: '25 Aug 2024 10:30 AM', toDate: '29 Aug 2024 11:30 AM'},
+  { noOfTrainees: 111111, department: 'Department 1', supervisor: 'Supervisor 1', location: 'Riyadh office', status: 'Accepted', fromDate: '20 Aug 2024 10:30 AM', toDate: '21 Aug 2024 11:30 AM' },
+  { noOfTrainees: 222222, department: 'Department 2', supervisor: 'Supervisor 2', location: 'Pune office', status: 'Under Review', fromDate: '21 Aug 2024 10:30 AM', toDate: '25 Aug 2024 11:30 AM' },
+  { noOfTrainees: 333333, department: 'Department 3', supervisor: 'Supervisor 3', location: 'Noida Office', status: 'Submitted', fromDate: '22 Aug 2024 10:30 AM', toDate: '26 Aug 2024 11:30 AM' },
+  { noOfTrainees: 444444, department: 'Department 4', supervisor: 'Supervisor 4', location: 'India office', status: 'Approved', fromDate: '23 Aug 2024 10:30 AM', toDate: '27 Aug 2024 11:30 AM' },
+  { noOfTrainees: 555555, department: 'Department 5', supervisor: 'Supervisor 5', location: 'UAE office', status: 'Closed', fromDate: '24 Aug 2024 10:30 AM', toDate: '28 Aug 2024 11:30 AM' },
+  { noOfTrainees: 666666, department: 'Department 6', supervisor: 'Supervisor 6', location: 'KSA Office', status: 'Rejected', fromDate: '25 Aug 2024 10:30 AM', toDate: '29 Aug 2024 11:30 AM' },
 ];
 
 const ScheduleClassTraining = () => {
@@ -117,97 +117,99 @@ const ScheduleClassTraining = () => {
   const renderNewRequestContent = () => (
     <>
       <View style={styles.container}>
-      {loading && (
-        <View style={styles.loaderContainer}>
-          <ActivityIndicator size="large" color="#0000ff" />
+        {loading && (
+          <View style={styles.loaderContainer}>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        )}
+        <Text style={styles.label}>Department Name</Text>
+        <TextInput style={styles.input} value={department}
+          autoCorrect={false} spellCheck={false}
+          onChangeText={setDepartment} />
+
+        <Text style={styles.label}>Supervisor Name</Text>
+        <TextInput style={styles.input} value={supervisor}
+          autoCorrect={false} spellCheck={false}
+          onChangeText={setSupervisor} />
+
+        <Text style={styles.label}>Training on Date & Time</Text>
+        <View style={styles.row}>
+          <TouchableOpacity onPress={() => setShowFromDatePicker(true)} style={styles.dateInput}>
+            <Text>{fromDate ? formatDateTime(fromDate) : 'From Date'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setShowToDatePicker(true)} style={styles.dateInput}>
+            <Text>{toDate ? formatDateTime(toDate) : 'To Date'}</Text>
+          </TouchableOpacity>
         </View>
-      )}
-      <Text style={styles.label}>Department Name</Text>
-      <TextInput style={styles.input} value={department} 
-      autoCorrect={false} spellCheck={false} 
-      onChangeText={setDepartment} />
 
-      <Text style={styles.label}>Supervisor Name</Text>
-      <TextInput style={styles.input} value={supervisor} 
-      autoCorrect={false} spellCheck={false} 
-      onChangeText={setSupervisor} />
+        {showFromDatePicker && (
+          <DateTimePicker
+            value={fromDate || new Date()}
+            mode="datetime"
+            display="default"
+            onChange={onFromDateChange}
+            minimumDate={new Date()} // Today's date
+            maximumDate={new Date(new Date().setFullYear(new Date().getFullYear() + 1))} // One year from today
+          />
+        )}
 
-      <Text style={styles.label}>Training on Date & Time</Text>
-      <View style={styles.row}>
-        <TouchableOpacity onPress={() => setShowFromDatePicker(true)} style={styles.dateInput}>
-          <Text>{fromDate ? formatDateTime(fromDate) : 'From Date'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => setShowToDatePicker(true)} style={styles.dateInput}>
-          <Text>{toDate ? formatDateTime(toDate) : 'To Date'}</Text>
-        </TouchableOpacity>
+        {showToDatePicker && (
+          <DateTimePicker
+            value={toDate || new Date()}
+            mode="datetime"
+            display="default"
+            onChange={onToDateChange}
+            minimumDate={fromDate || new Date()} // From date or today's date
+            maximumDate={new Date(new Date().setFullYear(new Date().getFullYear() + 1))} // One year from today
+          />
+        )}
+
+        <Text style={styles.label}>No. of Trainees</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          autoCorrect={false} spellCheck={false}
+          value={noOfTrainees}
+          onChangeText={handleNoOfTraineesChange}
+          onEndEditing={handleNoOfTraineesEndEditing}
+        />
+
+        <Text style={styles.label}>Location</Text>
+        <TextInput style={styles.input} value={location}
+          autoCorrect={false} spellCheck={false}
+          onChangeText={setLocation} />
       </View>
-
-      {showFromDatePicker && (
-        <DateTimePicker
-          value={fromDate || new Date()}
-          mode="datetime"
-          display="default"
-          onChange={onFromDateChange}
-          maximumDate={toDate || undefined}
-        />
-      )}
-
-      {showToDatePicker && (
-        <DateTimePicker
-          value={toDate || new Date()}
-          mode="datetime"
-          display="default"
-          onChange={onToDateChange}
-          minimumDate={fromDate || undefined}
-        />
-      )}
-
-      <Text style={styles.label}>No. of Trainees</Text>
-      <TextInput
-        style={styles.input}
-        keyboardType="numeric"
-        autoCorrect={false} spellCheck={false}
-        value={noOfTrainees}
-        onChangeText={handleNoOfTraineesChange}
-        onEndEditing={handleNoOfTraineesEndEditing}
-      />
-
-      <Text style={styles.label}>Location</Text>
-      <TextInput style={styles.input} value={location} 
-      autoCorrect={false} spellCheck={false} 
-      onChangeText={setLocation} />
-    </View>
     </>
   );
 
   const renderHistoryRequestsContent = () => (
     <View style={styles.historyView}>
-    <TrainingList data={TrainingDataArr} />
+      <TrainingList data={TrainingDataArr} />
     </View>
   );
 
   return (
     <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-    <KeyboardAwareScrollView
+      <KeyboardAwareScrollView
         contentContainerStyle={styles.container}
         resetScrollToCoords={{ x: 0, y: 0 }}
         scrollEnabled>
-      <View style={styles.segmentedControlContainer}>
-        <SegmentedControl
-          values={['New Schedule', 'Training History']}
-          selectedIndex={selectedIndex}
-          onChange={(event) => {
-            setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
-          }}
-          style={styles.segmentedControl}
-          tintColor="rgba(2, 28, 52, 1.0)"
-          fontStyle={{ fontSize: 15.5, fontWeight: 'bold', color: '#fff' }}
-          backgroundColor="rgba(211, 211, 211, 1.0)"
-        />
-      </View>
-      <View style={styles.container}>
-        {selectedIndex === 0 ? renderNewRequestContent() : renderHistoryRequestsContent()}
-      </View>
+        <View style={styles.segmentedControlContainer}>
+          <SegmentedControl
+            values={['New Schedule', 'Training History']}
+            selectedIndex={selectedIndex}
+            onChange={(event) => {
+              setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
+            }}
+            style={styles.segmentedControl}
+            tintColor="rgba(2, 28, 52, 1.0)"
+            fontStyle={{ fontSize: 15.5, fontWeight: 'bold', color: '#fff' }}
+            backgroundColor="rgba(211, 211, 211, 1.0)"
+          />
+        </View>
+        <View style={styles.container}>
+          {selectedIndex === 0 ? renderNewRequestContent() : renderHistoryRequestsContent()}
+        </View>
       </KeyboardAwareScrollView>
       {selectedIndex === 0 && (
         <View style={styles.buttonContainer}>
