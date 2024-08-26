9c2bf863590fa466f533a82c0c91ccf2da8fc8b0
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-08-26 14:39:01 +0530

    Update PdfViewer.tsx

--- Screens/PdfViewer.tsx
+++ Screens/PdfViewer.tsx
@@ -32,7 +32,7 @@ const PdfViewer = () => {
   if (loading) {
     return (
       <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="large" color='rgba(2, 28, 52, 1.0)' />
       </View>
     );
   }
