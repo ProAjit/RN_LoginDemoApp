6df2fffada793e2cc1630487ed192a8da38c1951
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-08-26 11:39:17 +0530

    Update SafetyIncidentsList.tsx

--- Screens/EndroseSafety/SafetyIncidentsList.tsx
+++ Screens/EndroseSafety/SafetyIncidentsList.tsx
@@ -99,7 +99,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.0,
     // Shadow for Android
-    elevation: 5,
+    elevation: 4,
   },
   innerContainer: {
     flexDirection: 'row',
