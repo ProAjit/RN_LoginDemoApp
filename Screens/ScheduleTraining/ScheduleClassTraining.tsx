451ff389efd7c0c2c1e20c3ee06fb96a832e4403
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-08-28 18:22:58 +0530

    Update ScheduleClassTraining.tsx

--- Screens/ScheduleTraining/ScheduleClassTraining.tsx
+++ Screens/ScheduleTraining/ScheduleClassTraining.tsx
@@ -11,14 +11,14 @@ import bottomButtonStyles from '../../styles/bottomButtonStyles';
 import segmentStyle from '../../styles/segmentStyle';
 // import { Ionicons } from '@expo/vector-icons'; // Use Ionicons for the dropdown arrow
 
-const regionsData = ['Riyadh', 'India', 'UAE', 'Mumbai', 'Dubai'];
+const regionsData = ['Riyadh', 'Jeddah', 'Macca', 'Madina', 'Hessa'];
 
 const TrainingDataArr = [
   { noOfTrainees: 111111, department: 'Department 1', supervisor: 'Supervisor 1', location: 'Riyadh office', status: 'Rescheduled', fromDate: '20 Aug 2024 10:30 AM', toDate: '21 Aug 2024 11:30 AM' },
-  { noOfTrainees: 222222, department: 'Department 2', supervisor: 'Supervisor 2', location: 'Pune office', status: 'Under Review', fromDate: '21 Aug 2024 10:30 AM', toDate: '25 Aug 2024 11:30 AM' },
-  { noOfTrainees: 333333, department: 'Department 3', supervisor: 'Supervisor 3', location: 'Noida Office', status: 'On Hold', fromDate: '22 Aug 2024 10:30 AM', toDate: '26 Aug 2024 11:30 AM' },
-  { noOfTrainees: 444444, department: 'Department 4', supervisor: 'Supervisor 4', location: 'India office', status: 'Approved', fromDate: '23 Aug 2024 10:30 AM', toDate: '27 Aug 2024 11:30 AM' },
-  { noOfTrainees: 555555, department: 'Department 6', supervisor: 'Supervisor 6', location: 'KSA Office', status: 'Rejected', fromDate: '25 Aug 2024 10:30 AM', toDate: '29 Aug 2024 11:30 AM' },
+  { noOfTrainees: 222222, department: 'Department 2', supervisor: 'Supervisor 2', location: 'Jeddah office', status: 'Under Review', fromDate: '21 Aug 2024 10:30 AM', toDate: '25 Aug 2024 11:30 AM' },
+  { noOfTrainees: 333333, department: 'Department 3', supervisor: 'Supervisor 3', location: 'Macca Office', status: 'On Hold', fromDate: '22 Aug 2024 10:30 AM', toDate: '26 Aug 2024 11:30 AM' },
+  { noOfTrainees: 444444, department: 'Department 4', supervisor: 'Supervisor 4', location: 'Madina office', status: 'Approved', fromDate: '23 Aug 2024 10:30 AM', toDate: '27 Aug 2024 11:30 AM' },
+  { noOfTrainees: 555555, department: 'Department 6', supervisor: 'Supervisor 6', location: 'Hessa Office', status: 'Rejected', fromDate: '25 Aug 2024 10:30 AM', toDate: '29 Aug 2024 11:30 AM' },
 ];
 
 const ScheduleClassTraining = () => {
