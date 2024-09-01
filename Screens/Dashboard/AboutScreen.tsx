d0ff830875e04cf40be3beec006b0b8652f206e5
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-09-01 10:48:58 +0530

    Update AboutScreen.tsx

--- Screens/Dashboard/AboutScreen.tsx
+++ Screens/Dashboard/AboutScreen.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-const AboutScreen  = () => {
+const AboutScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>About Screen</Text>
+      {/* Top Image */}
+      <Image
+        source={{ uri: '/Users/ajitsatarkar/Documents/React_Native_Git/RN_LoginPOC/RN_LoginDemoApp/images/login/logo.png' }} // Replace with your image source
+        style={styles.image}
+      />
+
+      {/* Ministry Text */}
+      <Text style={styles.ministryText}>
+        Ministry of National Guard Health Affairs Safety Management - cR
+      </Text>
+
+      {/* Support Services Text */}
+      <Text style={styles.supportText}>
+        Support Services - Operations
+        {'\n'}<Text style={styles.riyadhText}>Riyadh</Text>
+      </Text>
+
+      {/* Email Address Text */}
+      <Text style={styles.emailText}>
+        <Text style={styles.boldText}>Email Address:</Text>{' '}
+        <Text style={styles.grayText}>test@ngha.com</Text>
+      </Text>
+
+      {/* Phone Number Text */}
+      <Text style={styles.phoneText}>
+        <Text style={styles.boldText}>Phone#</Text>{' '}
+        <Text style={styles.grayText}>055003551</Text>{' '}
+        <Text style={styles.boldText}>Ext#</Text>{' '}
+        <Text style={styles.grayText}>41228</Text>
+      </Text>
     </View>
   );
 };
@@ -12,9 +41,55 @@ const AboutScreen  = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 16,
     alignItems: 'center',
     backgroundColor: '#F4F6FF',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 16,
+    marginTop: 50,
+  },
+  ministryText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
+    textAlign: 'center',
+    marginBottom: 8,
+    marginTop: 25,
+  },
+  supportText: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: 'darkgray',
+    textAlign: 'center',
+    marginBottom: 8,
+    marginTop: 25,
+  },
+  riyadhText: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: 'darkgray',
+    textAlign: 'center',
+  },
+  emailText: {
+    fontSize: 14,
+    color: '#000',
+    marginBottom: 8,
+    marginTop: 25,
+  },
+  phoneText: {
+    fontSize: 14,
+    color: '#000',
+    marginTop: 25,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  grayText: {
+    color: 'gray',
   },
 });
 
