4b40bd0814921addc410ebc0f2b70696db1f9e36
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-09-15 10:30:40 +0530

    Update QueriesServices.tsx

--- Networking/QueriesServices.tsx
+++ Networking/QueriesServices.tsx
@@ -1,19 +1,19 @@
 import axios from 'axios';
-import { API } from '../Constants/GlobalData';
+import { API, USER } from '../Constants/GlobalData';
 
 export const submitQueriesData = async (name: string, email: string, phone: string,
   description: string, region: string, title: string, subject: string) => {
     
     const formData = new FormData();
-
     formData.append('Name', name);
     formData.append('Email', email);
     formData.append('Phone', phone);
-    formData.append('Description', description);
     formData.append('Title', title);
+    formData.append('Region', region);
     formData.append('Subject', subject);
+    formData.append('Description', description);
     formData.append('Status', 'New');
-    formData.append('BadgeId', 'BadgeId40');
+    formData.append('BadgeId', USER.badgeId);
 
   try {
     const response = await axios.post(`${API.TestBaseURL}/createQuery`, formData, {
