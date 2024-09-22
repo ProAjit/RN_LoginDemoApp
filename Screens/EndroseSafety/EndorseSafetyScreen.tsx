a0352bf20f494e00b5aa7d6755f826be5e77d642
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-09-22 08:31:48 +0530

    Update EndorseSafetyScreen.tsx
    camera actions

--- Screens/EndroseSafety/EndorseSafetyScreen.tsx
+++ Screens/EndroseSafety/EndorseSafetyScreen.tsx
@@ -4,7 +4,7 @@ import {
   StyleSheet, Alert, KeyboardAvoidingView, Switch, 
   FlatList, TouchableWithoutFeedback} from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-// import { launchCamera, CameraOptions } from 'react-native-image-picker';
+import { launchCamera, CameraOptions } from 'react-native-image-picker';
 import { submitSafetyEndorsement } from '../../Networking/EndorseSafety/EndorseSafetyServices';
 import SegmentedControl from '@react-native-segmented-control/segmented-control';
 import SafetyIncidentsList from './SafetyIncidentsList';
@@ -27,26 +27,26 @@ const EndorseSafetyScreen = () => {
   const singleton = AppSingleton.getInstance();
   const regionsData = ['Riyadh', 'Jeddah', 'Macca', 'Madina', 'Hessa'];
 
-  // const openCamera = () => {
-  //   const options: CameraOptions = {
-  //     mediaType: 'photo',
-  //     cameraType: 'back',
-  //     quality: 1,
-  //     includeBase64: true,
-  //     saveToPhotos: false,
-  //   };
+  const openCamera = () => {
+    const options: CameraOptions = {
+      mediaType: 'photo',
+      cameraType: 'back',
+      quality: 1,
+      includeBase64: true,
+      saveToPhotos: false,
+    };
 
-  //   launchCamera(options, (response) => {
-  //     if (response.didCancel) {
-  //       Alert.alert('User cancelled image picker');
-  //     } else if (response.errorCode) {
-  //       Alert.alert('ImagePicker Error: camera_unavailable');
-  //     } else if (response.assets) {
-  //       const uri = response.assets[0].uri;
-  //       setImage(uri || null);
-  //     }
-  //   });
-  // };
+    launchCamera(options, (response) => {
+      if (response.didCancel) {
+        Alert.alert('User cancelled image picker');
+      } else if (response.errorCode) {
+        Alert.alert('ImagePicker Error: camera_unavailable');
+      } else if (response.assets) {
+        const uri = response.assets[0].uri;
+        setImage(uri || null);
+      }
+    });
+  };
 
   const handleSubmit = async () => {
     if (region.trim() === '' || location.trim() === '' || description.trim() === '') {
@@ -73,8 +73,6 @@ const EndorseSafetyScreen = () => {
   };
 
   const handleCancel = () => {
-    setName('');
-    setBadgeNumber('');
     setLocation('');
     setDescription('');
     setImage(null);
@@ -127,8 +125,7 @@ const EndorseSafetyScreen = () => {
 
       <View style={styles.topView}>
         {image && <Image source={{ uri: image }} style={styles.imageView} />}
-        <Button title="Take A Picture" />
-        {/* onPress={openCamera} /> */}
+        <Button title="Take A Picture" onPress={openCamera} />
       </View>
 
       {/* New Switch View */}
