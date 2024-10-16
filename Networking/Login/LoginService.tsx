2cc69955ff461b43d399db58f818552cc75c5eb1
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-10-16 16:21:14 +0530

    Update LoginService.tsx
    included password crypto code

--- Networking/Login/LoginService.tsx
+++ Networking/Login/LoginService.tsx
@@ -1,12 +1,24 @@
 // LoginService.tsx
 import axios from 'axios';
 import { API } from '../../Constants/GlobalData';
+var CryptoJS = require('crypto-js'); 
+
+export const getEncryptedData = (value: string) => {
+  var key = CryptoJS.enc.Utf8.parse('NGHAMobileAp2015');
+  let iv = CryptoJS.lib.WordArray.create(key.words.slice(4, 16));
+  var cipherText = CryptoJS.AES.encrypt(value, key, {
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  });
+  return cipherText.toString();
+};
 
 export const loginApi = async (userName: string, password: string) => {
 
   const requestBody = {
     UserName: String(userName).toUpperCase(),
-    Password: 'EvLUHwePskmmbPOXBMbwag==',
+    Password: getEncryptedData(password),//'EvLUHwePskmmbPOXBMbwag==',
     InFuture1: `ANDROID`,
     InFuture2: '12~OPPO~CPH2471~~ANDROID',
     InFuture3: '-',
@@ -27,6 +39,7 @@ export const loginApi = async (userName: string, password: string) => {
     throw error;
   }
 };
+
 
 export const profileApi = async (userName: string, sessionId: string, sessionToken: string) => {
   const requestBody = {
